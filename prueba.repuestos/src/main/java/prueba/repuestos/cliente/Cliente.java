0890256f8ceec618e0ee69909805afbec8ab1425
NB <NB@NB-HP>
2017-05-17 11:36:42 -0400

    Clase cliente

---
+++ prueba.repuestos/src/main/java/prueba/repuestos/cliente/Cliente.java
@@ -0,0 +1,106 @@
+package prueba.repuestos.cliente;
+
+public class Cliente {
+	
+	//atributos
+	private Long Id;
+	private String nroDoc;
+	private String	tipoDoc;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	private String password;
+	
+	
+	//metodos
+	public Cliente(){
+		
+	}
+
+
+	public Cliente(Long id, String nroDoc, String tipoDoc, String nombre, String direccion, String telefono,
+			String password) {
+		super();
+		Id = id;
+		this.nroDoc = nroDoc;
+		this.tipoDoc = tipoDoc;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.password = password;
+	}
+
+
+	public Long getId() {
+		return Id;
+	}
+
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+
+	public String getNroDoc() {
+		return nroDoc;
+	}
+
+
+	public void setNroDoc(String nroDoc) {
+		this.nroDoc = nroDoc;
+	}
+
+
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+
+
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	}
+
